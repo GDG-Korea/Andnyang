fae306ef179c1555fd8bff47786fb36ebbddba5d
Leonardo YongUk Kim <dalinaum@gmail.com>
2013-01-22 21:13:47 +0900

    myWriter -> Channel

--- bot.go
+++ bot.go
@@ -69,13 +69,14 @@ func ParseLine(line string) []string {
 	return output
 }
 
-type myWriter struct {
-	*textproto.Writer
+type Channel struct {
+	writer *textproto.Writer
+	channel string 
 }
 
-func (w *myWriter) Talk(channel string, msg string) {
-	text := fmt.Sprintf("PRIVMSG %s :%s", channel, msg)
-	w.Writer.PrintfLine(text)
+func (c *Channel) Talk(msg string) {
+	text := fmt.Sprintf("PRIVMSG %s :%s", c.channel, msg)
+	c.writer.PrintfLine(text)
 }
 
 func main() {
@@ -87,7 +88,7 @@ func main() {
 	tpReader := textproto.NewReader(reader)
 	writer := bufio.NewWriter(conn)
 	tpWriter := textproto.NewWriter(writer)
-	m := myWriter { tpWriter }
+	channel := Channel { tpWriter, ircbot.channel }
 
 	userCommand := fmt.Sprintf("USER %s 8 * :%s\n", ircbot.user, ircbot.user)
 	tpWriter.PrintfLine(userCommand)
@@ -110,17 +111,17 @@ func main() {
 			tpWriter.PrintfLine(request)
 		} else if arr[0][0] == ':' && arr[1] == "PRIVMSG" && arr[2] == ircbot.channel && arr[3][1] == '!' {
 			fmt.Printf(">>> %s\n", line)
-			m.Talk(ircbot.channel, arr[3][2:])
+			channel.Talk(arr[3][2:])
 		} else if arr[0][0] == ':' && arr[1] == "JOIN" && arr[2][1:] == ircbot.channel {
 			fmt.Printf(">>> %s\n", line)
 			nameLine := arr[0][1:]
 			nameArr := strings.Split(nameLine, "!")
 			name := nameArr[0]
 			if name == ircbot.nick {
-				m.Talk(ircbot.channel, "오랜만이에요. :) 모두 안녕하세요.")
+				channel.Talk("오랜만이에요. :) 모두 안녕하세요.")
 			} else {
 				text := fmt.Sprintf("안녕하세요. %s님 ^^", name)
-				m.Talk(ircbot.channel, text)
+				channel.Talk(text)
 			}
 			request := fmt.Sprintf("MODE %s +o %s", ircbot.channel, name)
 			tpWriter.PrintfLine(request)
